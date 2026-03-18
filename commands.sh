c5c3f4bf9eb50faf15891fa37cb16c681a370878
TakibiFire <takibi3fire+github@gmail.com>
2026-03-18 11:32:46 +0900

    Enable livereload for mkdocs.

--- commands.sh
+++ commands.sh
@@ -54,7 +54,9 @@ while (( "$#" )); do
             # Kill existing tailwind process if running to avoid duplicates.
             pkill -f "@tailwindcss/cli" || true
             pnpm dlx @tailwindcss/cli -i ./src/input.css -o ./docs/stylesheets/tailwind.css --watch &
-            execute_cmd .venv/bin/mkdocs serve
+            # Use polling and explicit livereload flag to work around click/mac issues.
+            export WATCHDOG_USE_POLLING=1
+            execute_cmd .venv/bin/mkdocs serve --livereload
             ;;
         docs_build)
             pnpm dlx @tailwindcss/cli -i ./src/input.css -o ./docs/stylesheets/tailwind.css --minify
